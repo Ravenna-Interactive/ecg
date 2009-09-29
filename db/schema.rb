55bc026d40ceee903eb4bf5d0a315adc1157560c
Beau Collins <beaucollins@gmail.com>
2009-09-29 14:43:26 -0700

    db and fixture changes

--- db/schema.rb
+++ db/schema.rb
@@ -40,14 +40,6 @@ ActiveRecord::Schema.define(:version => 20090928235125) do
     t.datetime "updated_at"
   end
 
-  create_table "pages", :force => true do |t|
-    t.string   "title"
-    t.string   "name"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "photos", :force => true do |t|
     t.integer  "product_id"
     t.datetime "created_at"
