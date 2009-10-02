aeeabd585cb18bb9bbbaaabbca314bf099a0e3bf
Ravenna <tj@ravennainteractive.com>
2009-10-02 16:20:01 -0700

    mysql random

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
    before_filter :find_categories
   
   def home
-    @visitors = Visitor.find(:all, :order => "random()", :limit => 1)
+    @visitors = Visitor.find(:all, :order => "RAND()", :limit => 1)
     @categories = Category.all
     
   end
