322dfe779acb9695ce776661314f604bc67b2ddb
Ravenna <tj@ravennainteractive.com>
2009-10-14 12:58:24 -0700

    rand

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
    before_filter :find_categories
   
   def home
-    @visitors = Visitor.find(:all, :order => "Random()", :limit => 1)
+    @visitors = Visitor.find(:all, :order => "RAND()", :limit => 1)
     @categories = Category.all
     @new_spotlight = Product.find_new_spotlight
     @vintage_spotlight = Product.find_vintage_spotlight
