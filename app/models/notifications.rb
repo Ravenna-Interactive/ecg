1da569889d0276a092b3c28f81b6c67174fc53dc
Ravenna <tj@ravennainteractive.com>
2009-10-22 17:32:47 -0700

    contact forms working

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -2,7 +2,7 @@ class Notifications < ActionMailer::Base
     
     def wish_list(email_params, sent_at = Time.now)
       subject "[Wish List] " #<< email_params[:subject]
-      recipients "jayboone@emeraldcityguitars.com" # Perhaps your address here?
+      recipients "tj@ravennainteractive.com" # Perhaps your address here?
       from email_params[:address]
       sent_on sent_at
 
@@ -12,13 +12,14 @@ class Notifications < ActionMailer::Base
            :sender_name => email_params[:name],
            :product => email_params[:product],
            :phone => email_params[:phone],
-           :home => email_params[:home]
+           :home => email_params[:home],
+           :address => email_params[:address]
         
     end
     
      def contact_form(email_params, sent_at = Time.now)
         subject "[Contact Form] " #<< email_params[:subject]
-        recipients "jayboone@emeraldcityguitars.com" # Perhaps your address here?
+        recipients "tj@ravennainteractive.com" # Perhaps your address here?
         from email_params[:address]
         sent_on sent_at
 
@@ -28,7 +29,8 @@ class Notifications < ActionMailer::Base
              :sender_name => email_params[:name],
              :comment => email_params[:comment],
              :phone => email_params[:phone],
-             :home => email_params[:home]
+             :home => email_params[:home],
+             :address => email_params[:address]
 
       end
   end
