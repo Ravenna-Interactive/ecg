5edc551bab9bf907f06e038ba9d2be91edc46e83
Ravenna <tj@ravennainteractive.com>
2009-12-03 11:57:49 -0800

    bigger guest photo

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,7 +5,7 @@ class Visitor < ActiveRecord::Base
       :thickbox => "400x400>",
       :visitor => "150x150>", # Image to be the Visitor Potfolio
       :original => "258x198#" }, # Image to fit the homepage recents 258px x 198px
-     
+      :big => "1000x1000>",
       
       :url => "/system/visitors/:id/:basename_:style.:extension",
       :path => ":rails_root/public/system/visitors/:id/:basename_:style.:extension"
